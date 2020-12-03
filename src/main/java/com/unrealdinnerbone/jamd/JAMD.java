4f66d92d687309fdf71eef1588b30700030ee9c7
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2020-12-03 10:26:58 -0600

    Clear Stuff

--- src/main/java/com/unrealdinnerbone/jamd/JAMD.java
+++ src/main/java/com/unrealdinnerbone/jamd/JAMD.java
@@ -1,6 +1,7 @@
 package com.unrealdinnerbone.jamd;
 
 import com.unrealdinnerbone.jamd.data.DataEvent;
+import net.minecraft.entity.EntityClassification;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
@@ -21,6 +22,10 @@ public class JAMD
     public static final String MOD_ID = "jamd";
 
     private static final ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+    private static final ForgeConfigSpec.BooleanValue FLOWERS = builder.comment("Stop mods from adding custom flowers").define("flowers", true);
+    private static final ForgeConfigSpec.BooleanValue STRUCTURES = builder.comment("Stop mods from adding surface structures").define("surface_structures", true);
+    private static final ForgeConfigSpec.BooleanValue ENTITIES = builder.comment("Stop mods from adding entities").define("entities", true);
+    private static final ForgeConfigSpec.BooleanValue LAKES = builder.comment("Stop mods from adding lakes").define("lakes", true);
 
     public static final ResourceLocation DIM_ID = new ResourceLocation(MOD_ID, "mining");
 
@@ -34,8 +39,18 @@ public class JAMD
     public void onBiomesLoad(BiomeLoadingEvent biomeLoadingEvent) {
         if (biomeLoadingEvent.getName() != null && biomeLoadingEvent.getName().toString().equals(DIM_ID.toString())) {
             biomeLoadingEvent.getGeneration().getStructures().clear();
-            biomeLoadingEvent.getGeneration().getFeatures(GenerationStage.Decoration.VEGETAL_DECORATION).clear();
-            biomeLoadingEvent.getGeneration().getFeatures(GenerationStage.Decoration.SURFACE_STRUCTURES).clear();
+            if(FLOWERS.get()) {
+                biomeLoadingEvent.getGeneration().getFeatures(GenerationStage.Decoration.VEGETAL_DECORATION).clear();
+            }
+            if(STRUCTURES.get()) {
+                biomeLoadingEvent.getGeneration().getFeatures(GenerationStage.Decoration.SURFACE_STRUCTURES).clear();
+            }
+            if(ENTITIES.get()) {
+                biomeLoadingEvent.getSpawns().getSpawnerTypes().forEach(spawnerType -> biomeLoadingEvent.getSpawns().getSpawner(spawnerType).clear());
+            }
+            if(LAKES.get()) {
+                biomeLoadingEvent.getGeneration().getFeatures(GenerationStage.Decoration.LAKES).clear();
+            }
         }
     }
 }
